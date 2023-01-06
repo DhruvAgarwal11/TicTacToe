05cc8b403377a48c70837c7f515ffdcd22d3441c
Dhruv Agarwal <dhruvagarwal@gmail.com>
2023-01-05 23:37:40 -0800

    Highlights Squares upon winning

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 
 function Square(props){
     return (
-        <button className = "square" onClick = {props.onClick}>
+        <button className = {"square" + (props.highlight ? " square-winner": "")} onClick = {props.onClick}>
             {props.value}
         </button>
     );
@@ -16,6 +16,7 @@ class Board extends React.Component{
             <Square 
                 value = {this.props.squares[i]}
                 onClick = {() => this.props.onClick(i)}
+                highlight = {this.props.highlighted.includes(i)}
             />
         );
     }
@@ -87,7 +88,8 @@ class Game extends React.Component{
         });
         let status;
         if (winner) {
-            status = "Winner: " + winner;
+            status = "Winner: " + winner[0];
+
         }
         else{
             status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O');
@@ -99,6 +101,7 @@ class Game extends React.Component{
                     <Board 
                         squares = {current.squares}
                         onClick = {i => this.handleClick(i)}
+                        highlighted = {winner ? winner[1] : []}
                     />
                 </div>
                 <div className = "game-info">
@@ -127,7 +130,7 @@ function calculateWinner(squares) {
     for (var i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return [squares[a], lines[i]];
         }
     }
     return null;
